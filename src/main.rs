8efb3221489cb9803c71f9ea7519caa89100eabe
pokailin <pokailin17@gmail.com>
2024-11-01 18:05:53 +0800

    [ main ] simple example args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::{command, Parser};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Amount of expense - value greater than 0.0
+    #[arg(short, long, default_value_t = 0.0)]
+    amount: f32,
+
+    /// Amounts of expense - value greater than 0.0
+    #[arg(short = 'z', long, default_value_t = 0.0)]
+    amounts: f32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Hello {:?}!", args.amount);
 }
